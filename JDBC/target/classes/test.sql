8beb9c54263078861b21ae9af5b65d24a3ac4ca5
reestts <reestts@gmail.com>
2020-08-29 10:50:51 +0300

    Target delete

--- JDBC/target/classes/test.sql
+++
@@ -1,5 +0,0 @@
-#insert into users (name, age, email) values ('Alex', 22, 'alex@gmail.com');
-#insert into users (name, age, email) values ('Petr', 12, 'petr@gmail.com');
-#insert into users (name, age, email) values ('Ivan', 32, 'ivan@gmail.com');
-#select * from users;
-select * from animal;
