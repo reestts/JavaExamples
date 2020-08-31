2f0d033a772c8736e32dd679fb780eb5aa6aeccb
reestts <reestts@gmail.com>
2020-08-31 22:57:14 +0300

    Reorder methods

--- Core/src/main/java/com/reestts/interfaces/MainInterfaces.java
+++ Core/src/main/java/com/reestts/interfaces/MainInterfaces.java
@@ -1,13 +1,14 @@
 package com.reestts.interfaces;
 
 public class MainInterfaces implements Test {
-    public static void main(String[] args) {
-        Test.method3();
-    }
 
     @Override
     public void method1() {
 
+    }
+
+    public static void main(String[] args) {
+        Test.method3();
     }
 
     @Override
