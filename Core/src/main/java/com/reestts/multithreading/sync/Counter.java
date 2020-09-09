05a0165d8579095fe10825059ac2af25e390bca6
Iurii Goncharov <52718257+reestts@users.noreply.github.com>
2020-09-09 22:52:33 +0300

    +1 method

---
+++ Core/src/main/java/com/reestts/multithreading/sync/Counter.java
@@ -0,0 +1,5 @@
+package com.reestts.multithreading.sync;
+
+public class Counter {
+    static int count = 0;
+}
---
+++ Core/src/main/java/com/reestts/multithreading/sync/MainSync.java
@@ -0,0 +1,13 @@
+package com.reestts.multithreading.sync;
+
+public class MainSync {
+    public static void main(String[] args) {
+        TestThread thread1 = new TestThread();
+        TestThread thread2 = new TestThread();
+        TestThread thread3 = new TestThread();
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+}
