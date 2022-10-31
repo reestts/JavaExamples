a913e53d4fac099eab36ea02e49a7d46d5110d5b
Iurii Goncharov <52718257+reestts@users.noreply.github.com>
2022-10-31 10:51:36 +0400

    Add array utils class

---
+++ Core/src/main/java/com/reestts/utils/ArrayUtils.java
@@ -0,0 +1,23 @@
+package com.reestts.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ArrayUtils {
+
+    public static void outputArray(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            if (i == array.length - 1) {
+                System.out.print(array[i]);
+            } else {
+                System.out.print(array[i] + ", ");
+            }
+        }
+    }
+
+    public static void swapElements(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
