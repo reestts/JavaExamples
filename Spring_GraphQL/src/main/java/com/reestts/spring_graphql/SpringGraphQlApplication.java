fe1ad472e73b8cc82679040982d7e35a74d59aa0
Iurii Goncharov <52718257+reestts@users.noreply.github.com>
2022-11-03 18:58:03 +0400

    Add new module for Spring with GraphQL

---
+++ Spring_GraphQL/src/main/java/com/reestts/spring_graphql/SpringGraphQlApplication.java
@@ -0,0 +1,13 @@
+package com.reestts.spring_graphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGraphQlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGraphQlApplication.class, args);
+	}
+
+}
---
+++ Spring_GraphQL/src/test/java/com/reestts/spring_graphql/SpringGraphQlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reestts.spring_graphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGraphQlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
